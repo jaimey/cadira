1b86f5560859f35d3902c8603ff5892d4b435878
dogi <dogi.developer@gmail.com>
2020-07-24 11:33:02 +0200

    fix(App) Fixed error when loading the system by default with the admin user

---
+++ user_privileges/user_privileges_1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=true;
+
+$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'$2y$10$by0SOh84OVl59DeanRk9Uu6O96WX70lwaiXSQYcdD771aiwQVXQlG','confirm_password'=>'$2y$10$xI8bqG6Gdrf/Vll5F8b8lu7g7M6WS8VbVmTtjIcPLGxnrxhESO.YG','first_name'=>'Administrator','last_name'=>'System','roleid'=>'H2','email1'=>'kapsulexp@gmail.com','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'24','end_hour'=>'23:00','start_hour'=>'00:00','is_owner'=>'1','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'m6RVNLQzg6LPOAau','time_zone'=>'Europe/Brussels','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'if_male_628288.png','internal_mailer'=>'1','theme'=>'alphagrey','language'=>'en_us','reminder_interval'=>'1 Minute','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'1','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'1','rowheight'=>'','defaulteventstatus'=>'Planned','defaultactivitytype'=>'Call','hidecompletedevents'=>'0','defaultcalendarview'=>'MyCalendar','defaultlandingpage'=>'Home','currency_name'=>'Euro','currency_code'=>'EUR','currency_symbol'=>'&#8364;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'1');
+
+?>
