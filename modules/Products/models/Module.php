4ec5fa518c417b263e49e27f699265cc71a3d70c
dogi <dogi.developer@gmail.com>
2020-07-24 15:59:51 +0200

    feat(Products) Activation of registration overview in module Products

--- modules/Products/models/Module.php
+++ modules/Products/models/Module.php
@@ -6,10 +6,10 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 
-class Products_Module_Model extends Vtiger_Module_Model {
-
+class Products_Module_Model extends Vtiger_Module_Model
+{
 	/**
 	 * Function to get list view query for popup window
 	 * @param <String> $sourceModule Parent module
@@ -18,36 +18,37 @@ class Products_Module_Model extends Vtiger_Module_Model {
 	 * @param <String> $listQuery
 	 * @return <String> Listview Query
 	 */
-	public function getQueryByModuleField($sourceModule, $field, $record, $listQuery) {
-		$supportedModulesList = array($this->getName(), 'Vendors', 'Leads', 'Accounts', 'Contacts', 'Potentials');
+	public function getQueryByModuleField($sourceModule, $field, $record, $listQuery)
+	{
+		$supportedModulesList = [$this->getName(), 'Vendors', 'Leads', 'Accounts', 'Contacts', 'Potentials'];
 		if (($sourceModule == 'PriceBooks' && $field == 'priceBookRelatedList')
 				|| in_array($sourceModule, $supportedModulesList)
 				|| in_array($sourceModule, getInventoryModules())) {
-
-			$condition = " vtiger_products.discontinued = 1 ";
-            		$db = PearDatabase::getInstance();
-		    	$params = array($record);
+			$condition = ' vtiger_products.discontinued = 1 ';
+			$db = PearDatabase::getInstance();
+			$params = [$record];
 			if ($sourceModule === $this->getName()) {
-				$condition .= " AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_seproductsrel WHERE setype = '". $this->getName(). "' UNION SELECT crmid FROM vtiger_seproductsrel WHERE productid = ?) AND vtiger_products.productid <> ? ";
-                		$params = array($record, $record);
+				$condition .= " AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_seproductsrel WHERE setype = '".$this->getName()."' UNION SELECT crmid FROM vtiger_seproductsrel WHERE productid = ?) AND vtiger_products.productid <> ? ";
+				$params = [$record, $record];
 			} elseif ($sourceModule === 'PriceBooks') {
-				$condition .= " AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_pricebookproductrel WHERE pricebookid = ?) ";
+				$condition .= ' AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_pricebookproductrel WHERE pricebookid = ?) ';
 			} elseif ($sourceModule === 'Vendors') {
-				$condition .= " AND vtiger_products.vendor_id != ? ";
+				$condition .= ' AND vtiger_products.vendor_id != ? ';
 			} elseif (in_array($sourceModule, $supportedModulesList)) {
-				$condition .= " AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_seproductsrel WHERE crmid = ?)";
+				$condition .= ' AND vtiger_products.productid NOT IN (SELECT productid FROM vtiger_seproductsrel WHERE crmid = ?)';
 			} else {
-                $params = array();
+				$params = [];
 			}
 			$condition = $db->convert2Sql($condition, $params);
 
 			$pos = stripos($listQuery, 'where');
 			if ($pos) {
 				$split = preg_split('/where/i', $listQuery);
-				$overRideQuery = $split[0] . ' WHERE ' . $split[1] . ' AND ' . $condition;
+				$overRideQuery = $split[0].' WHERE '.$split[1].' AND '.$condition;
 			} else {
-				$overRideQuery = $listQuery. ' WHERE ' . $condition;
+				$overRideQuery = $listQuery.' WHERE '.$condition;
 			}
+
 			return $overRideQuery;
 		}
 	}
@@ -57,58 +58,66 @@ class Products_Module_Model extends Vtiger_Module_Model {
 	 * @param <type> $relatedModule
 	 * @return <type>
 	 */
-	public function getSpecificRelationQuery($relatedModule) {
+	public function getSpecificRelationQuery($relatedModule)
+	{
 		if ($relatedModule === 'Leads') {
-			$specificQuery = 'AND vtiger_leaddetails.converted = 0';
-			return $specificQuery;
+			return 'AND vtiger_leaddetails.converted = 0';
 		}
+
 		return parent::getSpecificRelationQuery($relatedModule);
- 	}
+	}
 
 	/**
 	 * Function to get prices for specified products with specific currency
 	 * @param <Integer> $currenctId
 	 * @param <Array> $productIdsList
+	 * @param mixed $currencyId
+	 * @param mixed $skipActualPrice
 	 * @return <Array>
 	 */
-	public function getPricesForProducts($currencyId, $productIdsList, $skipActualPrice = false) {
+	public function getPricesForProducts($currencyId, $productIdsList, $skipActualPrice = false)
+	{
 		return getPricesForProducts($currencyId, $productIdsList, $this->getName(), $skipActualPrice);
 	}
-	
+
 	/**
 	 * Function to check whether the module is summary view supported
 	 * @return <Boolean> - true/false
 	 */
-	public function isSummaryViewSupported() {
-		return false;
+	public function isSummaryViewSupported()
+	{
+		return true;
 	}
-	
+
 	/**
 	 * Function searches the records in the module, if parentId & parentModule
 	 * is given then searches only those records related to them.
 	 * @param <String> $searchValue - Search value
 	 * @param <Integer> $parentId - parent recordId
 	 * @param <String> $parentModule - parent module name
+	 * @param mixed $relatedModule
 	 * @return <Array of Vtiger_Record_Model>
 	 */
-	public function searchRecord($searchValue, $parentId=false, $parentModule=false, $relatedModule=false) {
-		if(!empty($searchValue) && empty($parentId) && empty($parentModule) && (in_array($relatedModule, getInventoryModules()))) {
+	public function searchRecord($searchValue, $parentId = false, $parentModule = false, $relatedModule = false)
+	{
+		if (! empty($searchValue) && empty($parentId) && empty($parentModule) && (in_array($relatedModule, getInventoryModules()))) {
 			$matchingRecords = Products_Record_Model::getSearchResult($searchValue, $this->getName());
-		}else {
+		} else {
 			return parent::searchRecord($searchValue);
 		}
 
 		return $matchingRecords;
 	}
-	
+
 	/**
 	 * Function returns query for Product-PriceBooks relation
 	 * @param <Vtiger_Record_Model> $recordModel
 	 * @param <Vtiger_Record_Model> $relatedModuleModel
 	 * @return <String>
 	 */
-	function get_product_pricebooks($recordModel, $relatedModuleModel) {
-		$query = 'SELECT vtiger_pricebook.pricebookid, vtiger_pricebook.bookname, vtiger_pricebook.active, vtiger_crmentity.crmid, 
+	public function get_product_pricebooks($recordModel, $relatedModuleModel)
+	{
+		return 'SELECT vtiger_pricebook.pricebookid, vtiger_pricebook.bookname, vtiger_pricebook.active, vtiger_crmentity.crmid, 
 						vtiger_crmentity.smownerid, vtiger_pricebookproductrel.listprice, vtiger_products.unit_price
 					FROM vtiger_pricebook
 					INNER JOIN vtiger_pricebookproductrel ON vtiger_pricebook.pricebookid = vtiger_pricebookproductrel.pricebookid
@@ -117,29 +126,29 @@ class Products_Module_Model extends Vtiger_Module_Model {
 					INNER JOIN vtiger_pricebookcf on vtiger_pricebookcf.pricebookid = vtiger_pricebook.pricebookid
 					LEFT JOIN vtiger_users ON vtiger_users.id=vtiger_crmentity.smownerid
 					LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid '
-					. Users_Privileges_Model::getNonAdminAccessControlQuery($relatedModuleModel->getName()) .'
+					.Users_Privileges_Model::getNonAdminAccessControlQuery($relatedModuleModel->getName()).'
 					WHERE vtiger_products.productid = '.$recordModel->getId().' and vtiger_crmentity.deleted = 0';
-					
-		return $query;
 	}
-    
-    /**
+
+	/**
 	 * Function returns export query which included currency_id field
 	 * @param <String> $where
+	 * @param mixed $focus
+	 * @param mixed $query
 	 * @return <String> export query
 	 */
-	public function getExportQuery($focus, $query) {
+	public function getExportQuery($focus, $query)
+	{
 		$baseTableName = $focus->table_name;
 		$splitQuery = preg_split('/ FROM /i', $query);
 		$columnFields = explode(',', $splitQuery[0]);
-        $columnFields[] = ' vtiger_currency_info.currency_name AS currency_id, crmid';
+		$columnFields[] = ' vtiger_currency_info.currency_name AS currency_id, crmid';
 
-		$joinSplit = preg_split('/ WHERE /i',$splitQuery[1]);
-		$joinSplit[0] .= " LEFT JOIN vtiger_currency_info ON vtiger_currency_info.id = $baseTableName.currency_id";
-		$splitQuery[1] = $joinSplit[0].' WHERE ' .$joinSplit[1];
+		$joinSplit = preg_split('/ WHERE /i', $splitQuery[1]);
+		$joinSplit[0] .= " LEFT JOIN vtiger_currency_info ON vtiger_currency_info.id = ${baseTableName}.currency_id";
+		$splitQuery[1] = $joinSplit[0].' WHERE '.$joinSplit[1];
 
-		$query = implode(',', $columnFields).' FROM '.$splitQuery[1];
-		return $query;
+		return implode(',', $columnFields).' FROM '.$splitQuery[1];
 	}
 
 	/**
@@ -148,7 +157,8 @@ class Products_Module_Model extends Vtiger_Module_Model {
 	 * @param <String> $relatedModule
 	 * @return <Array> $matchedRecordModels
 	 */
-	public function searchRecordsOnSequenceNumber($searchValue, $relatedModule) {
+	public function searchRecordsOnSequenceNumber($searchValue, $relatedModule)
+	{
 		if (in_array($relatedModule, getInventoryModules())) {
 			$db = PearDatabase::getInstance();
 			$moduleName = $this->getName();
@@ -160,31 +170,31 @@ class Products_Module_Model extends Vtiger_Module_Model {
 				$fieldName = 'service_no';
 			}
 
-			$query = "SELECT label, crmid, $fieldName FROM vtiger_crmentity
-						INNER JOIN $tableName ON $tableName.$baseFieldName = vtiger_crmentity.crmid
-						WHERE $fieldName LIKE ? AND vtiger_crmentity.deleted = 0 AND discontinued = 1";
-			$result = $db->pquery($query, array("%$searchValue%"));
+			$query = "SELECT label, crmid, ${fieldName} FROM vtiger_crmentity
+						INNER JOIN ${tableName} ON ${tableName}.${baseFieldName} = vtiger_crmentity.crmid
+						WHERE ${fieldName} LIKE ? AND vtiger_crmentity.deleted = 0 AND discontinued = 1";
+			$result = $db->pquery($query, ["%${searchValue}%"]);
 			$noOfRows = $db->num_rows($result);
 
-			$matchingRecords = array();
-			for($i=0; $i<$noOfRows; ++$i) {
+			$matchingRecords = [];
+			for ($i = 0; $i < $noOfRows; ++$i) {
 				$row = $db->query_result_rowdata($result, $i);
-				if(Users_Privileges_Model::isPermitted($row['setype'], 'DetailView', $row['crmid'])) {
+				if (Users_Privileges_Model::isPermitted($row['setype'], 'DetailView', $row['crmid'])) {
 					$row['id'] = $row['crmid'];
 					$modelClassName = Vtiger_Loader::getComponentClassName('Model', 'Record', $moduleName);
 					$recordInstance = new $modelClassName();
 					$matchingRecords[$row['id']] = $recordInstance->setData($row)->setModuleFromInstance($this);
 				}
 			}
+
 			return $matchingRecords;
 		}
 	}
 
-	/*
-	 * Function to get supported utility actions for a module
-	 */
-	function getUtilityActionsNames() {
-		return array('Import', 'Export', 'DuplicatesHandling');
+	// Function to get supported utility actions for a module
+	public function getUtilityActionsNames()
+	{
+		return ['Import', 'Export', 'DuplicatesHandling'];
 	}
 
 	/**
@@ -192,10 +202,11 @@ class Products_Module_Model extends Vtiger_Module_Model {
 	 * @param <Array> $productIdsList
 	 * @return <Array>
 	 */
-	public function areChildProducts($productIdsList = array()) {
+	public function areChildProducts($productIdsList = [])
+	{
 		if ($productIdsList) {
-			if (!is_array($productIdsList)) {
-				$productIdsList = array($productIdsList);
+			if (! is_array($productIdsList)) {
+				$productIdsList = [$productIdsList];
 			}
 
 			$db = PearDatabase::getInstance();
@@ -206,12 +217,12 @@ class Products_Module_Model extends Vtiger_Module_Model {
 
 			$result = $db->pquery($query, $productIdsList);
 
-			$childProductIdsList = array();
-			while($rowData = $db->fetch_array($result)) {
+			$childProductIdsList = [];
+			while ($rowData = $db->fetch_array($result)) {
 				$childProductIdsList[] = $rowData['crmid'];
 			}
 
-			$childProductsResult = array();
+			$childProductsResult = [];
 			foreach ($productIdsList as $productId) {
 				$isChildProduct = false;
 				if (in_array($productId, $childProductIdsList)) {
@@ -224,8 +235,9 @@ class Products_Module_Model extends Vtiger_Module_Model {
 		}
 	}
 
-	public function getAdditionalImportFields() {
-		if (!$this->importableFields) {
+	public function getAdditionalImportFields()
+	{
+		if (! $this->importableFields) {
 			$taxModels = Inventory_TaxRecord_Model::getProductTaxes();
 			foreach ($taxModels as $taxId => $taxModel) {
 				if ($taxModel->isDeleted()) {
@@ -233,7 +245,7 @@ class Products_Module_Model extends Vtiger_Module_Model {
 				}
 			}
 
-			$taxHeaders = array();
+			$taxHeaders = [];
 			$allRegions = Inventory_TaxRegion_Model::getAllTaxRegions();
 			foreach ($taxModels as $taxId => $taxModel) {
 				$tax = $taxModel->get('taxname');
@@ -242,16 +254,16 @@ class Products_Module_Model extends Vtiger_Module_Model {
 
 				$regions = $taxModel->getRegionTaxes();
 				foreach ($regions as $regionsTaxInfo) {
-					foreach(array_fill_keys($regionsTaxInfo['list'], $regionsTaxInfo['value']) as $regionId => $taxPercentage) {
+					foreach (array_fill_keys($regionsTaxInfo['list'], $regionsTaxInfo['value']) as $regionId => $taxPercentage) {
 						if ($allRegions[$regionId]) {
 							$taxRegionName = $taxName.'-'.$allRegions[$regionId]->getName();
-							$taxHeaders[$tax."_$regionId"] = decode_html($taxRegionName);
+							$taxHeaders[$tax."_${regionId}"] = decode_html($taxRegionName);
 						}
 					}
 				}
 			}
 
-			$this->importableFields = array();
+			$this->importableFields = [];
 			foreach ($taxHeaders as $fieldName => $fieldLabel) {
 				$fieldModel = new Vtiger_Field_Model();
 				$fieldModel->name = $fieldName;
@@ -264,21 +276,23 @@ class Products_Module_Model extends Vtiger_Module_Model {
 				$this->importableFields[$fieldName] = $fieldModel;
 			}
 		}
+
 		return $this->importableFields;
 	}
 
 	/**
 	 * Function to get popup view fields
 	 */
-	public function getPopupViewFieldsList(){
+	public function getPopupViewFieldsList()
+	{
 		$summaryFieldsList = parent::getPopupViewFieldsList();
-		foreach($summaryFieldsList as $key=>$fieldname){
-			if($fieldname == 'qty_per_unit'){
+		foreach ($summaryFieldsList as $key=>$fieldname) {
+			if ($fieldname == 'qty_per_unit') {
 				$out = array_splice($summaryFieldsList, $key, 1);
 				array_splice($summaryFieldsList, 1, 0, $out);
 			}
 		}
+
 		return $summaryFieldsList;
 	}
-
 }
--- modules/Products/views/Detail.php
+++ modules/Products/views/Detail.php
@@ -8,47 +8,78 @@
  * All Rights Reserved.
  * *********************************************************************************** */
 
-class Products_Detail_View extends Vtiger_Detail_View {
-
-	public function __construct() {
+class Products_Detail_View extends Vtiger_Detail_View
+{
+	public function __construct()
+	{
 		parent::__construct();
 		$this->exposeMethod('showBundleTotalCostView');
 	}
-	
-	public function requiresPermission(Vtiger_Request $request){
+
+	public function requiresPermission(Vtiger_Request $request)
+	{
 		$permissions = parent::requiresPermission($request);
 		$mode = $request->getMode();
-		if(!empty($mode)) {
+		if (! empty($mode)) {
 			switch ($mode) {
 				case 'showBundleTotalCostView':
-					$permissions[] = array('module_parameter' => 'module', 'action' => 'DetailView', 'record_parameter' => 'record');
-					$permissions[] = array('module_parameter' => 'relatedModule', 'action' => 'DetailView');
+					$permissions[] = ['module_parameter' => 'module', 'action' => 'DetailView', 'record_parameter' => 'record'];
+					$permissions[] = ['module_parameter' => 'relatedModule', 'action' => 'DetailView'];
+
 					break;
 			}
 		}
+
 		return $permissions;
 	}
-	
-	function preProcess(Vtiger_Request $request, $display = true) {
+
+	public function showModuleSummaryView($request)
+	{
+		$recordId = $request->get('record');
+		$moduleName = $request->getModule();
+
+		if (! $this->record) {
+			$this->record = Vtiger_DetailView_Model::getInstance($moduleName, $recordId);
+		}
+		$recordModel = $this->record->getRecord();
+		$recordStrucure = Vtiger_RecordStructure_Model::getInstanceFromRecordModel($recordModel, Vtiger_RecordStructure_Model::RECORD_STRUCTURE_MODE_SUMMARY);
+
+		$moduleModel = $recordModel->getModule();
+		$viewer = $this->getViewer($request);
+		$viewer->assign('RECORD', $recordModel);
+		$viewer->assign('BLOCK_LIST', $moduleModel->getBlocks());
+		$viewer->assign('USER_MODEL', Users_Record_Model::getCurrentUserModel());
+
+		$viewer->assign('MODULE_NAME', $moduleName);
+		$viewer->assign('IS_AJAX_ENABLED', $this->isAjaxEnabled($recordModel));
+		$viewer->assign('SUMMARY_RECORD_STRUCTURE', $recordStrucure->getStructure());
+		$viewer->assign('RELATED_ACTIVITIES', $this->getActivities($request));
+
+		return $viewer->view('ModuleSummaryView.tpl', $moduleName, true);
+	}
+
+	public function preProcess(Vtiger_Request $request, $display = true)
+	{
 		$recordId = $request->get('record');
 		$moduleName = $request->getModule();
 
 		$recordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
 		$baseCurrenctDetails = $recordModel->getBaseCurrencyDetails();
-		
+
 		$viewer = $this->getViewer($request);
 		$viewer->assign('BASE_CURRENCY_SYMBOL', $baseCurrenctDetails['symbol']);
-		
+
 		parent::preProcess($request, $display);
 	}
 
-	public function showModuleDetailView(Vtiger_Request $request) {
+	public function showModuleDetailView(Vtiger_Request $request)
+	{
 		$recordId = $request->get('record');
 		$moduleName = $request->getModule();
 
 		$recordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
 		$baseCurrenctDetails = $recordModel->getBaseCurrencyDetails();
-		
+
 		$viewer = $this->getViewer($request);
 		$viewer->assign('BASE_CURRENCY_SYMBOL', $baseCurrenctDetails['symbol']);
 		$viewer->assign('TAXCLASS_DETAILS', $recordModel->getTaxClassDetails());
@@ -57,46 +88,44 @@ class Products_Detail_View extends Vtiger_Detail_View {
 		return parent::showModuleDetailView($request);
 	}
 
-	public function showModuleBasicView(Vtiger_Request $request) {
-		return $this->showModuleDetailView($request);
-	}
-	
-	public function getOverlayHeaderScripts(Vtiger_Request $request){
+	public function getOverlayHeaderScripts(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$moduleDetailFile = 'modules.'.$moduleName.'.resources.Detail';
-		$jsFileNames = array(
+		$jsFileNames = [
 			'~libraries/jquery/boxslider/jquery.bxslider.min.js',
 			'modules.PriceBooks.resources.Detail',
-		);
+		];
 		$jsFileNames[] = $moduleDetailFile;
-		$jsScriptInstances = $this->checkAndConvertJsScripts($jsFileNames);
-		return $jsScriptInstances;	
+
+		return $this->checkAndConvertJsScripts($jsFileNames);
 	}
 
-	public function getHeaderScripts(Vtiger_Request $request) {
+	public function getHeaderScripts(Vtiger_Request $request)
+	{
 		$headerScriptInstances = parent::getHeaderScripts($request);
 		$moduleName = $request->getModule();
 		$moduleDetailFile = 'modules.'.$moduleName.'.resources.Detail';
 		$moduleRelatedListFile = 'modules.'.$moduleName.'.resources.RelatedList';
-		unset($headerScriptInstances[$moduleDetailFile]);
-		unset($headerScriptInstances[$moduleRelatedListFile]);
+		unset($headerScriptInstances[$moduleDetailFile], $headerScriptInstances[$moduleRelatedListFile]);
 
-		$jsFileNames = array(
+		$jsFileNames = [
 			'~libraries/jquery/jquery.cycle.min.js',
-			'~libraries/jquery/boxslider/jquery.bxslider.min.js', 
+			'~libraries/jquery/boxslider/jquery.bxslider.min.js',
 			'modules.PriceBooks.resources.Detail',
 			'modules.PriceBooks.resources.RelatedList',
-		);
-		
+		];
+
 		$jsFileNames[] = $moduleDetailFile;
 		$jsFileNames[] = $moduleRelatedListFile;
 
 		$jsScriptInstances = $this->checkAndConvertJsScripts($jsFileNames);
-		$headerScriptInstances = array_merge($headerScriptInstances, $jsScriptInstances);
-		return $headerScriptInstances;
+
+		return array_merge($headerScriptInstances, $jsScriptInstances);
 	}
-	
-	public function showBundleTotalCostView(Vtiger_Request $request) {
+
+	public function showBundleTotalCostView(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$relatedModuleName = $request->get('relatedModule');
 		$parentRecordId = $request->get('record');
