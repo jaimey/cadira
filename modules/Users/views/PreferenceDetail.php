5caaaddb068d1aa0f4650967d9e577451419ceb1
dogi <dogi.developer@gmail.com>
2020-07-27 17:23:09 +0200

    fmt(Users) Update format PSR2

--- modules/Users/views/PreferenceDetail.php
+++ modules/Users/views/PreferenceDetail.php
@@ -6,19 +6,21 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 
-class Users_PreferenceDetail_View extends Vtiger_Detail_View {
-
-	public function requiresPermission(\Vtiger_Request $request) {
-		return array();
+class Users_PreferenceDetail_View extends Vtiger_Detail_View
+{
+	public function requiresPermission(\Vtiger_Request $request)
+	{
+		return [];
 	}
-    
-    public function checkPermission(Vtiger_Request $request) {
+
+	public function checkPermission(Vtiger_Request $request)
+	{
 		$currentUserModel = Users_Record_Model::getCurrentUserModel();
 		$record = $request->get('record');
 
-		if($currentUserModel->isAdminUser() == true || $currentUserModel->get('id') == $record) {
+		if ($currentUserModel->isAdminUser() == true || $currentUserModel->get('id') == $record) {
 			return true;
 		} else {
 			throw new AppException(vtranslate('LBL_PERMISSION_DENIED'));
@@ -30,7 +32,8 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 	 * @param <type> $request
 	 * @return <String>
 	 */
-	public function preProcessTplName(Vtiger_Request $request) {
+	public function preProcessTplName(Vtiger_Request $request)
+	{
 		return 'PreferenceDetailViewPreProcess.tpl';
 	}
 
@@ -38,12 +41,14 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 	 * Function shows basic detail for the record
 	 * @param <type> $request
 	 */
-	public function showModuleBasicView($request) {
+	public function showModuleBasicView($request)
+	{
 		return $this->showModuleDetailView($request);
 	}
 
-	public function preProcess(Vtiger_Request $request, $display=true) {
-		if($this->checkPermission($request)) {
+	public function preProcess(Vtiger_Request $request, $display = true)
+	{
+		if ($this->checkPermission($request)) {
 			$qualifiedModuleName = $request->getModule(false);
 			$currentUser = Users_Record_Model::getCurrentUserModel();
 			$recordId = $request->get('record');
@@ -52,7 +57,7 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$recordModel = $detailViewModel->getRecord();
 			$selectedModuleMenuCategory = 'MARKETING';
 
-			$detailViewLinkParams = array('MODULE'=>$moduleName,'RECORD'=>$recordId);
+			$detailViewLinkParams = ['MODULE'=>$moduleName, 'RECORD'=>$recordId];
 			$detailViewLinks = $detailViewModel->getDetailViewLinks($detailViewLinkParams);
 
 			$viewer = $this->getViewer($request);
@@ -64,12 +69,12 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$viewer->assign('IS_EDITABLE', $detailViewModel->getRecord()->isEditable($moduleName));
 			$viewer->assign('IS_DELETABLE', $detailViewModel->getRecord()->isDeletable($moduleName));
 
-			$linkParams = array('MODULE'=>$moduleName, 'ACTION'=>$request->get('view'));
+			$linkParams = ['MODULE'=>$moduleName, 'ACTION'=>$request->get('view')];
 			$linkModels = $detailViewModel->getSideBarLinks($linkParams);
 			$viewer->assign('QUICK_LINKS', $linkModels);
 			$viewer->assign('PAGETITLE', $this->getPageTitle($request));
-			$viewer->assign('SCRIPTS',$this->getHeaderScripts($request));
-			$viewer->assign('STYLES',$this->getHeaderCss($request));
+			$viewer->assign('SCRIPTS', $this->getHeaderScripts($request));
+			$viewer->assign('STYLES', $this->getHeaderCss($request));
 			$viewer->assign('LANGUAGE_STRINGS', $this->getJSLanguageStrings($request));
 			$viewer->assign('SEARCHABLE_MODULES', Vtiger_Module_Model::getSearchableModules());
 
@@ -78,11 +83,11 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$menuStructure = Vtiger_MenuStructure_Model::getInstanceFromMenuList($menuModelsList, $selectedModule);
 
 			// Order by pre-defined automation process for QuickCreate.
-			uksort($menuModelsList, array('Vtiger_MenuStructure_Model', 'sortMenuItemsByProcess'));
+			uksort($menuModelsList, ['Vtiger_MenuStructure_Model', 'sortMenuItemsByProcess']);
 
 			$companyDetails = Vtiger_CompanyDetails_Model::getInstanceById();
 			$companyLogo = $companyDetails->getLogo();
-			$viewer->assign('SELECTED_MENU_CATEGORY',$selectedModuleMenuCategory);
+			$viewer->assign('SELECTED_MENU_CATEGORY', $selectedModuleMenuCategory);
 			$viewer->assign('CURRENTDATE', date('Y-n-j'));
 			$viewer->assign('MODULE', $selectedModule);
 			$viewer->assign('QUALIFIED_MODULE', $qualifiedModuleName);
@@ -93,27 +98,27 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$viewer->assign('MENU_STRUCTURE', $menuStructure);
 			$viewer->assign('MENU_SELECTED_MODULENAME', $selectedModule);
 			$viewer->assign('MENU_TOPITEMS_LIMIT', $menuStructure->getLimit());
-			$viewer->assign('COMPANY_LOGO',$companyLogo);
+			$viewer->assign('COMPANY_LOGO', $companyLogo);
 			$viewer->assign('USER_MODEL', Users_Record_Model::getCurrentUserModel());
 
 			$homeModuleModel = Vtiger_Module_Model::getInstance('Home');
 			$viewer->assign('HOME_MODULE_MODEL', $homeModuleModel);
-			$viewer->assign('HEADER_LINKS',$this->getHeaderLinks());
+			$viewer->assign('HEADER_LINKS', $this->getHeaderLinks());
 			$viewer->assign('ANNOUNCEMENT', $this->getAnnouncement());
 			$viewer->assign('CURRENT_VIEW', $request->get('view'));
 			$viewer->assign('SKIN_PATH', Vtiger_Theme::getCurrentUserThemePath());
 			$viewer->assign('CURRENT_USER_MODEL', $currentUser);
 			$viewer->assign('LANGUAGE', $currentUser->get('language'));
-			$viewer->assign('COMPANY_DETAILS_SETTINGS',new Settings_Vtiger_CompanyDetails_Model());
+			$viewer->assign('COMPANY_DETAILS_SETTINGS', new Settings_Vtiger_CompanyDetails_Model());
 
-			$settingsModel = Settings_Vtiger_Module_Model::getInstance(); 
-			$menuModels = $settingsModel->getMenus(); 
+			$settingsModel = Settings_Vtiger_Module_Model::getInstance();
+			$menuModels = $settingsModel->getMenus();
 
-			if(!empty($selectedMenuId)) {
+			if (! empty($selectedMenuId)) {
 				$selectedMenu = Settings_Vtiger_Menu_Model::getInstanceById($selectedMenuId);
-			} elseif(!empty($moduleName) && $moduleName != 'Vtiger') {
-				$fieldItem = Settings_Vtiger_Index_View::getSelectedFieldFromModule($menuModels,$moduleName);
-				if($fieldItem){
+			} elseif (! empty($moduleName) && $moduleName != 'Vtiger') {
+				$fieldItem = Settings_Vtiger_Index_View::getSelectedFieldFromModule($menuModels, $moduleName);
+				if ($fieldItem) {
 					$selectedMenu = Settings_Vtiger_Menu_Model::getInstanceById($fieldItem->get('blockid'));
 					$fieldId = $fieldItem->get('fieldid');
 				} else {
@@ -127,10 +132,10 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 				$selectedMenu = $menuModels[$firstKey];
 			}
 
-			$settingsMenItems = array();
-			foreach($menuModels as $menuModel) {
+			$settingsMenItems = [];
+			foreach ($menuModels as $menuModel) {
 				$menuItems = $menuModel->getMenuItems();
-				foreach($menuItems as $menuItem) {
+				foreach ($menuItems as $menuItem) {
 					$settingsMenItems[$menuItem->get('name')] = $menuItem;
 				}
 			}
@@ -139,7 +144,7 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
 
 			$moduleFields = $moduleModel->getFields();
-			foreach($moduleFields as $fieldName => $fieldModel){
+			foreach ($moduleFields as $fieldName => $fieldModel) {
 				$fieldsInfo[$fieldName] = $fieldModel->getFieldInfo();
 			}
 			$viewer->assign('FIELDS_INFO', json_encode($fieldsInfo));
@@ -147,18 +152,14 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 			$activeBLock = Settings_Vtiger_Module_Model::getActiveBlockName($request);
 			$viewer->assign('ACTIVE_BLOCK', $activeBLock);
 
-			if($display) {
+			if ($display) {
 				$this->preProcessDisplay($request);
 			}
 		}
 	}
 
-	protected function preProcessDisplay(Vtiger_Request $request) {
-		$viewer = $this->getViewer($request);
-		$viewer->view($this->preProcessTplName($request), $request->getModule());
-	}
-
-	public function process(Vtiger_Request $request) {
+	public function process(Vtiger_Request $request)
+	{
 		$recordId = $request->get('record');
 		$moduleName = $request->getModule();
 
@@ -169,30 +170,37 @@ class Users_PreferenceDetail_View extends Vtiger_Detail_View {
 		$dayStartPicklistValues = Users_Record_Model::getDayStartsPicklistValues($recordStructureInstance->getStructure());
 
 		$viewer = $this->getViewer($request);
-		$viewer->assign("DAY_STARTS", Zend_Json::encode($dayStartPicklistValues));
+		$viewer->assign('DAY_STARTS', Zend_Json::encode($dayStartPicklistValues));
+
 		$viewer->assign('IMAGE_DETAILS', $recordModel->getImageDetails());
 
 		return parent::process($request);
 	}
 
-	public function getHeaderScripts(Vtiger_Request $request) {
+	public function getHeaderScripts(Vtiger_Request $request)
+	{
 		$headerScriptInstances = parent::getHeaderScripts($request);
 		$moduleName = $request->getModule();
 		$moduleDetailFile = 'modules.'.$moduleName.'.resources.PreferenceDetail';
 		unset($headerScriptInstances[$moduleDetailFile]);
 
-		$jsFileNames = array(
-			"modules.Users.resources.Detail",
-			"modules.Users.resources.Users",
+		$jsFileNames = [
+			'modules.Users.resources.Detail',
+			'modules.Users.resources.Users',
 			'modules.'.$moduleName.'.resources.PreferenceDetail',
 			'modules.'.$moduleName.'.resources.PreferenceEdit',
 			'modules.Settings.Vtiger.resources.Index',
-			"~layouts/v7/lib/jquery/Lightweight-jQuery-In-page-Filtering-Plugin-instaFilta/instafilta.min.js"
-		);
+			'~layouts/v7/lib/jquery/Lightweight-jQuery-In-page-Filtering-Plugin-instaFilta/instafilta.min.js'
+		];
 
 		$jsScriptInstances = $this->checkAndConvertJsScripts($jsFileNames);
-		$headerScriptInstances = array_merge($headerScriptInstances, $jsScriptInstances);
-		return $headerScriptInstances;
+
+		return array_merge($headerScriptInstances, $jsScriptInstances);
 	}
 
+	protected function preProcessDisplay(Vtiger_Request $request)
+	{
+		$viewer = $this->getViewer($request);
+		$viewer->view($this->preProcessTplName($request), $request->getModule());
+	}
 }
