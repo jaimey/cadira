89d560408c95f4edfe07f3c2c6cb2dd5e51c7ec7
dogi <dogi.developer@gmail.com>
2020-08-03 18:02:21 +0200

    fix(Dashboards) Fixed problem with PHP warning

--- modules/HelpDesk/dashboards/OpenTickets.php
+++ modules/HelpDesk/dashboards/OpenTickets.php
@@ -6,18 +6,21 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 
-class HelpDesk_OpenTickets_Dashboard extends Vtiger_IndexAjax_View {
-    
-    function getSearchParams($value) {
-        $listSearchParams = array();
-        $conditions = array(array('ticketstatus','e','Open'),array('assigned_user_id','e',decode_html(urlencode(escapeSlashes($value)))));
-        $listSearchParams[] = $conditions;
-        return '&search_params='. json_encode($listSearchParams);
-    }
+class HelpDesk_OpenTickets_Dashboard extends Vtiger_IndexAjax_View
+{
+	public function getSearchParams($value)
+	{
+		$listSearchParams = [];
+		$conditions = [['ticketstatus', 'e', 'Open'], ['assigned_user_id', 'e', decode_html(urlencode(escapeSlashes($value)))]];
+		$listSearchParams[] = $conditions;
 
-	public function process(Vtiger_Request $request) {
+		return '&search_params='.json_encode($listSearchParams);
+	}
+
+	public function process(Vtiger_Request $request)
+	{
 		$currentUser = Users_Record_Model::getCurrentUserModel();
 		$viewer = $this->getViewer($request);
 		$moduleName = $request->getModule();
@@ -26,22 +29,22 @@ class HelpDesk_OpenTickets_Dashboard extends Vtiger_IndexAjax_View {
 
 		$moduleModel = Vtiger_Module_Model::getInstance($moduleName);
 		$data = $moduleModel->getOpenTickets();
-        $listViewUrl = $moduleModel->getListViewUrlWithAllFilter();
-        for($i = 0;$i<count($data);$i++){
-            $data[$i]["links"] = $listViewUrl.$this->getSearchParams($data[$i][name]).'&nolistcache=1';
-        }
+		$listViewUrl = $moduleModel->getListViewUrlWithAllFilter();
+		for ($i = 0;$i < count($data);$i++) {
+			$data[$i]['links'] = $listViewUrl.$this->getSearchParams($data[$i]['name']).'&nolistcache=1';
+		}
 
 		$widget = Vtiger_Widget_Model::getInstance($linkId, $currentUser->getId());
 
 		$viewer->assign('WIDGET', $widget);
 		$viewer->assign('MODULE_NAME', $moduleName);
 		$viewer->assign('DATA', $data);
-        
+
 		//Include special script and css needed for this widget
 		$viewer->assign('CURRENTUSER', $currentUser);
 
 		$content = $request->get('content');
-		if(!empty($content)) {
+		if (! empty($content)) {
 			$viewer->view('dashboards/DashBoardWidgetContents.tpl', $moduleName);
 		} else {
 			$viewer->view('dashboards/OpenTickets.tpl', $moduleName);
