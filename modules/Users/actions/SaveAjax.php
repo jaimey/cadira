bfb846bcdcd5af8724b5cc63d371a533dd089628
dogi <dogi.developer@gmail.com>
2020-08-29 15:03:48 +0200

    fmt(Users) PSR2 format code update

--- modules/Users/actions/SaveAjax.php
+++ modules/Users/actions/SaveAjax.php
@@ -6,12 +6,13 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 vimport('~~/include/Webservices/Custom/ChangePassword.php');
 
-class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
-
-	function __construct() {
+class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action
+{
+	public function __construct()
+	{
 		parent::__construct();
 		$this->exposeMethod('userExists');
 		$this->exposeMethod('savePassword');
@@ -20,55 +21,58 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 		$this->exposeMethod('changeUsername');
 		$this->exposeMethod('changeAccessKey');
 	}
-    
-    public function requiresPermission(\Vtiger_Request $request) {
-		return array();
+
+	public function requiresPermission(\Vtiger_Request $request)
+	{
+		return [];
 	}
 
-	public function checkPermission(Vtiger_Request $request) {
+	public function checkPermission(Vtiger_Request $request)
+	{
 		$currentUserModel = Users_Record_Model::getCurrentUserModel();
 
 		$userId = $request->get('userid');
-		if(!$currentUserModel->isAdminUser()) {
+		if (! $currentUserModel->isAdminUser()) {
 			$mode = $request->getMode();
-			if($mode == 'savePassword' && (isset($userId) && $currentUserModel->getId() != $userId)) {
+			if ($mode == 'savePassword' && (isset($userId) && $currentUserModel->getId() != $userId)) {
 				throw new AppException(vtranslate('LBL_PERMISSION_DENIED', 'Vtiger'));
-			} else if(in_array($mode, array('userExists','restoreUser','transferOwner','changeUsername'))) {
+			} elseif (in_array($mode, ['userExists', 'restoreUser', 'transferOwner', 'changeUsername'])) {
 				throw new AppException(vtranslate('LBL_PERMISSION_DENIED', 'Vtiger'));
-			} else if($mode != 'savePassword' && ($currentUserModel->getId() != $request->get('record'))) {
+			} elseif ($mode != 'savePassword' && ($currentUserModel->getId() != $request->get('record'))) {
 				throw new AppException(vtranslate('LBL_PERMISSION_DENIED', 'Vtiger'));
 			}
 		}
 	}
 
-	public function process(Vtiger_Request $request) {
-
+	public function process(Vtiger_Request $request)
+	{
 		$mode = $request->get('mode');
-		if (!empty($mode)) {
+		if (! empty($mode)) {
 			$this->invokeExposedMethod($mode, $request);
+
 			return;
 		}
 
 		$recordModel = $this->saveRecord($request);
 
 		$fieldModelList = $recordModel->getModule()->getFields();
-		$result = array();
+		$result = [];
 		foreach ($fieldModelList as $fieldName => $fieldModel) {
-			if($fieldModel->isViewable()){
+			if ($fieldModel->isViewable()) {
 				$fieldValue = $displayValue = Vtiger_Util_Helper::toSafeHTML($recordModel->get($fieldName));
 				if ($fieldModel->getFieldDataType() !== 'currency') {
 					$displayValue = $fieldModel->getDisplayValue($fieldValue, $recordModel->getId());
 				}
-				if($fieldName == 'language') {
-					$displayValue =  Vtiger_Language_Handler::getLanguageLabel($fieldValue);
+				if ($fieldName == 'language') {
+					$displayValue = Vtiger_Language_Handler::getLanguageLabel($fieldValue);
 				}
-				if(($fieldName == 'currency_decimal_separator' || $fieldName == 'currency_grouping_separator') && ($displayValue == '&nbsp;')) {
+				if (($fieldName == 'currency_decimal_separator' || $fieldName == 'currency_grouping_separator') && ($displayValue == '&nbsp;')) {
 					$displayValue = vtranslate('Space', 'Users');
 				}
-                if($fieldName == 'defaultlandingpage'){
-                    $displayValue = vtranslate($fieldValue, $fieldValue);
-                }
-				$result[$fieldName] = array('value' => $fieldValue, 'display_value' => $displayValue);
+				if ($fieldName == 'defaultlandingpage') {
+					$displayValue = vtranslate($fieldValue, $fieldValue);
+				}
+				$result[$fieldName] = ['value' => $fieldValue, 'display_value' => $displayValue];
 			}
 		}
 
@@ -86,20 +90,24 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 	 * @param Vtiger_Request $request
 	 * @return Vtiger_Record_Model or Module specific Record Model instance
 	 */
-	public function getRecordModelFromRequest(Vtiger_Request $request) {
+	public function getRecordModelFromRequest(Vtiger_Request $request)
+	{
+		global $log;
+		$log->fatal('getRecordModelFromRequest');
+
 		$moduleName = $request->getModule();
 		$recordId = $request->get('record');
 		$currentUserModel = Users_Record_Model::getCurrentUserModel();
 
-		if(!empty($recordId)) {
+		if (! empty($recordId)) {
 			$recordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
 			$modelData = $recordModel->getData();
 			$recordModel->set('id', $recordId);
 			$sharedType = $request->get('sharedtype');
-			if(!empty($sharedType))
+			if (! empty($sharedType)) {
 				$recordModel->set('calendarsharedtype', $request->get('sharedtype'));
+			}
 			$recordModel->set('mode', 'edit');
-			
 		} else {
 			$recordModel = Vtiger_Record_Model::getCleanInstance($moduleName);
 			$modelData = $recordModel->getData();
@@ -108,75 +116,79 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 
 		foreach ($modelData as $fieldName => $value) {
 			$requestFieldExists = false;
-			 if($request->has($fieldName)){
-				$fieldValue = $request->get($fieldName,null);
+			if ($request->has($fieldName)) {
+				$fieldValue = $request->get($fieldName, null);
 				$requestFieldExists = true;
-			}else if($fieldName === $request->get('field')){
+			} elseif ($fieldName === $request->get('field')) {
 				$fieldValue = $request->get('value');
 				$requestFieldExists = true;
 			}
-			
-			if(!$requestFieldExists){
+
+			if (! $requestFieldExists) {
 				continue;
 			}
-			
-			if ($fieldName === 'is_admin' && (!$currentUserModel->isAdminUser() || !$fieldValue)) {
+
+			if ($fieldName === 'is_admin' && (! $currentUserModel->isAdminUser() || ! $fieldValue)) {
 				$fieldValue = 'off';
-			}else if($fieldName === 'is_admin' && $currentUserModel->isAdminUser()) {
-				$fieldValue = $fieldValue==1?"on":"off";
+			} elseif ($fieldName === 'is_admin' && $currentUserModel->isAdminUser()) {
+				$fieldValue = $fieldValue == 1 ? 'on' : 'off';
 			}
 
 			//to not update is_owner from ui
-			 if ($fieldName == 'is_owner' || (in_array($fieldName, array('user_type', 'roleid')) && !$currentUserModel->isAdminUser())) {
+			if ($fieldName == 'is_owner' || (in_array($fieldName, ['user_type', 'roleid']) && ! $currentUserModel->isAdminUser())) {
 				$fieldValue = null;
 			}
-			if($fieldName == 'signature'){
+			if ($fieldName == 'signature') {
 				$requestData = $request->getAll();
 				$instyle = new InStyle();
 				$signature = $instyle->convertStylesToInlineCss($requestData['signature']);
 				//#4823970 - Added to remove any action tags like <form>, <input>, <button>..
 				$fieldValue = vtlib_purify($signature);
 				// Purify malicious html event attributes
-	                        $fieldValue = purifyHtmlEventAttributes($fieldValue, true);
+				$fieldValue = purifyHtmlEventAttributes($fieldValue, true);
 			}
-			if($fieldValue !== null) {
-				if(!is_array($fieldValue)) {
+			if ($fieldValue !== null) {
+				if (! is_array($fieldValue)) {
 					$fieldValue = trim($fieldValue);
 				}
 				$recordModel->set($fieldName, $fieldValue);
 			}
 		}
-                $homePageComponents = $recordModel->getHomePageComponents();
-		$selectedHomePageComponents = $request->get('homepage_components', array());
+		$homePageComponents = $recordModel->getHomePageComponents();
+		$selectedHomePageComponents = $request->get('homepage_components', []);
 		foreach ($homePageComponents as $key => $value) {
-			if(in_array($key, $selectedHomePageComponents)) {
+			if (in_array($key, $selectedHomePageComponents)) {
 				$request->setGlobal($key, $key);
 			} else {
 				$request->setGlobal($key, '');
 			}
 		}
-		if($request->has('tagcloudview')) {
+		if ($request->has('tagcloudview')) {
 			// Tag cloud save
 			$tagCloud = $request->get('tagcloudview');
-			if($tagCloud == "on") {
+			if ($tagCloud == 'on') {
 				$recordModel->set('tagcloud', 0);
 			} else {
 				$recordModel->set('tagcloud', 1);
 			}
 		}
+
 		return $recordModel;
 	}
 
+	public function getOwnerRoleValue(Vtiger_Request $request, $fieldName)
+	{
+		$recordId = $request->get('record');
+		$moduleName = $request->getModule();
+		if (! empty($recordId)) {
+			$existingRecordModel = Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
 
-        public function getOwnerRoleValue(Vtiger_Request $request, $fieldName) {
-            $recordId = $request->get('record');
-            $moduleName = $request->getModule();
-            if(!empty($recordId)) {
-                    $existingRecordModel =  Vtiger_Record_Model::getInstanceById($recordId, $moduleName);
-                    return $existingRecordModel->get($fieldName);
-            }
-        }
-        public function userExists(Vtiger_Request $request){
+			return $existingRecordModel->get($fieldName);
+		}
+	}
+
+	public function userExists(Vtiger_Request $request)
+	{
 		$module = $request->getModule();
 		$userName = $request->get('user_name');
 		$status = Users_Record_Model::isUserExists($userName);
@@ -185,7 +197,8 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 		$response->emit();
 	}
 
-	public function savePassword(Vtiger_Request $request) {
+	public function savePassword(Vtiger_Request $request)
+	{
 		$module = $request->getModule();
 		$userModel = vglobal('current_user');
 		$newPassword = $request->get('new_password');
@@ -203,42 +216,42 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 		$response->emit();
 	}
 
-		/*
-		 * To restore a user
-		 * @param Vtiger_Request Object
-		 */
-		public function restoreUser(Vtiger_Request $request) {
-			$moduleName = $request->getModule();
-			$record = $request->get('userid');
-
-			$recordModel = Users_Record_Model::getInstanceById($record, $moduleName);
-				$recordModel->set('status', 'Active');
-				$recordModel->set('id', $record);
-				$recordModel->set('mode', 'edit');
-				$recordModel->save();
-
-				$db = PearDatabase::getInstance();
-				$db->pquery("UPDATE vtiger_users SET deleted=? WHERE id=?", array(0,$record));
-
-				$userModuleModel = Users_Module_Model::getInstance($moduleName);
-				$listViewUrl = $userModuleModel->getListViewUrl();
-
-			$response = new Vtiger_Response();
-			$response->setResult(array('message'=>vtranslate('LBL_USER_RESTORED_SUCCESSFULLY', $moduleName), 'listViewUrl' => $listViewUrl));
-			$response->emit();
-		}
-
 	/*
-	 * Function to transfer CRM owner without deleting User
+	 * To restore a user
+	 * @param Vtiger_Request Object
 	 */
-	public function transferOwner(Vtiger_Request $request) {
+	public function restoreUser(Vtiger_Request $request)
+	{
+		$moduleName = $request->getModule();
+		$record = $request->get('userid');
+
+		$recordModel = Users_Record_Model::getInstanceById($record, $moduleName);
+		$recordModel->set('status', 'Active');
+		$recordModel->set('id', $record);
+		$recordModel->set('mode', 'edit');
+		$recordModel->save();
+
+		$db = PearDatabase::getInstance();
+		$db->pquery('UPDATE vtiger_users SET deleted=? WHERE id=?', [0, $record]);
+
+		$userModuleModel = Users_Module_Model::getInstance($moduleName);
+		$listViewUrl = $userModuleModel->getListViewUrl();
+
+		$response = new Vtiger_Response();
+		$response->setResult(['message'=>vtranslate('LBL_USER_RESTORED_SUCCESSFULLY', $moduleName), 'listViewUrl' => $listViewUrl]);
+		$response->emit();
+	}
+
+	// Function to transfer CRM owner without deleting User
+	public function transferOwner(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule(false);
 		$record = $request->get('record');
 		$usersInstance = CRMEntity::getInstance($moduleName);
 		$status = $usersInstance->transferOwnership($record);
 		$response = new Vtiger_Response();
-		if($status) {
-			$response->setResult(array('message' => vtranslate('LBL_OWNERSHIP_TRANSFERRED_SUCCESSFULLY', $moduleName)));
+		if ($status) {
+			$response->setResult(['message' => vtranslate('LBL_OWNERSHIP_TRANSFERRED_SUCCESSFULLY', $moduleName)]);
 		} else {
 			$response->setError(vtranslate('LBL_OWNERSHIP_TRANSFERRED_FAILED', $moduleName));
 		}
@@ -248,24 +261,27 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 	/**
 	 * Function to change username
 	 */
-	public function changeUsername(Vtiger_Request $request) {
+	public function changeUsername(Vtiger_Request $request)
+	{
 		$response = new Vtiger_Response();
 		$userId = $request->get('userid');
 
 		$status = Users_Record_Model::changeUsername($request->get('newUsername'), $request->get('newPassword'), $request->get('oldPassword'), $userId);
-		if($status['success']) {
+		if ($status['success']) {
 			$response->setResult($status['message']);
-		}else{
+		} else {
 			$response->setError($status['message']);
 		}
 		$response->emit();
 	}
 
-	public function changeAccessKey(Vtiger_Request $request) {
+	public function changeAccessKey(Vtiger_Request $request)
+	{
 		$recordId = $request->get('record');
 		$moduleName = $request->getModule();
 
 		$response = new Vtiger_Response();
+
 		try {
 			$recordModel = Users_Record_Model::getInstanceById($recordId, $moduleName);
 			$oldAccessKey = $recordModel->get('accesskey');
@@ -273,7 +289,7 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 			$entity = $recordModel->getEntity();
 			$entity->createAccessKey();
 
-			require_once('modules/Users/CreateUserPrivilegeFile.php');
+			require_once 'modules/Users/CreateUserPrivilegeFile.php';
 			createUserPrivilegesfile($recordId);
 			Vtiger_AccessControl::clearUserPrivileges($recordId);
 
@@ -281,7 +297,7 @@ class Users_SaveAjax_Action extends Vtiger_SaveAjax_Action {
 			$newAccessKey = $recordModel->get('accesskey');
 
 			if ($newAccessKey != $oldAccessKey) {
-				$response->setResult(array('success' => true, 'message' => vtranslate('LBL_ACCESS_KEY_UPDATED_SUCCESSFULLY', $moduleName), 'accessKey' => $newAccessKey));
+				$response->setResult(['success' => true, 'message' => vtranslate('LBL_ACCESS_KEY_UPDATED_SUCCESSFULLY', $moduleName), 'accessKey' => $newAccessKey]);
 			} else {
 				$response->setError(vtranslate('LBL_FAILED_TO_UPDATE_ACCESS_KEY', $moduleName));
 			}
