437420bcb7399b1147247e49d61322952a241a4c
dogi <dogi.developer@gmail.com>
2020-11-21 21:52:38 +0100

    feat(Logger) Implemented logger to analyze the cron process and all the events performed by the Workflows. PSR2 format adaptation and code error resolution

--- include/logging.php
+++ include/logging.php
@@ -1,5 +1,5 @@
 <?php
-/*********************************************************************************
+/*
  * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
  * ("License"); You may not use this file except in compliance with the
  * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
@@ -11,18 +11,18 @@
  * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
  * All Rights Reserved.
  * Contributor(s): ______________________________________.
- ********************************************************************************/
-/*********************************************************************************
+ */
+/*
  * $Header: /advent/projects/wesat/vtiger_crm/sugarcrm/include/logging.php,v 1.1 2004/08/17 13:23:37 gjayakrishnan Exp $
  * Description:  Kicks off log4php.
- ********************************************************************************/
+ */
 
-require_once('config.php');
+require_once 'config.php';
 
 // Performance Optimization: Configure the log folder
-@include_once('config.performance.php');
+@include_once 'config.performance.php';
 global $PERFORMANCE_CONFIG;
-if(isset($PERFORMANCE_CONFIG) && isset($PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) && $PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) {
+if (isset($PERFORMANCE_CONFIG, $PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) && $PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) {
 	define('LOG4PHP_DIR', 'libraries/log4php.debug');
 } else {
 	define('LOG4PHP_DIR', 'libraries/log4php');
@@ -30,10 +30,13 @@ if(isset($PERFORMANCE_CONFIG) && isset($PERFORMANCE_CONFIG['LOG4PHP_DEBUG']) && 
 // END
 define('LOG4PHP_DEFAULT_INIT_OVERRIDE', true);
 
-require_once(LOG4PHP_DIR.'/LoggerManager.php');
-require_once(LOG4PHP_DIR.'/LoggerPropertyConfigurator.php');
+require_once LOG4PHP_DIR.'/LoggerManager.php';
+require_once LOG4PHP_DIR.'/LoggerPropertyConfigurator.php';
 
 $config = new LoggerPropertyConfigurator();
 $config->configure('log4php.properties');
 
-?>
+global $logbg;
+if (empty($logbg)) {
+	$logbg = LoggerManager::getLogger('BACKGROUND');
+}
