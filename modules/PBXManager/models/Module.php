218cbd38cec92f765366792da6e15ad6c74ddd3a
dogi <dogi.developer@gmail.com>
2020-11-21 21:18:11 +0100

    fmt(PBXManager) PSR2 code format update

--- modules/PBXManager/models/Module.php
+++ modules/PBXManager/models/Module.php
@@ -6,36 +6,42 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 
-class PBXManager_Module_Model extends Vtiger_Module_Model {
-
+class PBXManager_Module_Model extends Vtiger_Module_Model
+{
 	/**
 	 * Function to check whether the module is an entity type module or not
 	 * @return <Boolean> true/false
 	 */
-	public function isQuickCreateSupported() {
+	public function isQuickCreateSupported()
+	{
 		//PBXManager module is not enabled for quick create
 		return false;
 	}
 
 	/**
 	 * Overided to make editview=false for this module
+	 * @param mixed $actionName
 	 */
-	public function isPermitted($actionName) {
-		if($actionName == 'EditView')
+	public function isPermitted($actionName)
+	{
+		if ($actionName == 'EditView') {
 			return false;
-		else
+		} else {
 			return ($this->isActive() && Users_Privileges_Model::isPermitted($this->getName(), $actionName));
+		}
 	}
 
-	public function getModuleBasicLinks() {
+	public function getModuleBasicLinks()
+	{
 		$basicLinks = parent::getModuleBasicLinks();
 		foreach ($basicLinks as $key => $basicLink) {
-			if (in_array($basicLink['linklabel'], array('LBL_ADD_RECORD', 'LBL_IMPORT'))) {
+			if (in_array($basicLink['linklabel'], ['LBL_ADD_RECORD', 'LBL_IMPORT'])) {
 				unset($basicLinks[$key]);
 			}
 		}
+
 		return $basicLinks;
 	}
 
@@ -43,46 +49,49 @@ class PBXManager_Module_Model extends Vtiger_Module_Model {
 	 * Function to get Settings links
 	 * @return <Array>
 	 */
-	public function getSettingLinks(){
-		if(!$this->isEntityModule()) {
-			return array();
+	public function getSettingLinks()
+	{
+		if (! $this->isEntityModule()) {
+			return [];
 		}
 
-		$settingsLinks = array();
+		$settingsLinks = [];
 		$currentUser = Users_Record_Model::getCurrentUserModel();
-		if($currentUser->isAdminUser()) {
+		if ($currentUser->isAdminUser()) {
 			vimport('~~modules/com_vtiger_workflow/VTWorkflowUtils.php');
 
-			$layoutEditorImagePath = Vtiger_Theme::getImagePath('LayoutEditor.gif');
 			$editWorkflowsImagePath = Vtiger_Theme::getImagePath('EditWorkflows.png');
 
-			if(VTWorkflowUtils::checkModuleWorkflow($this->getName())) {
-				$settingsLinks[] = array(
-						'linktype' => 'LISTVIEWSETTING',
-						'linklabel' => 'LBL_EDIT_WORKFLOWS',
-						'linkurl' => 'index.php?parent=Settings&module=Workflows&view=List&sourceModule='.$this->getName(),
-						'linkicon' => $editWorkflowsImagePath
-				);
+			if (VTWorkflowUtils::checkModuleWorkflow($this->getName())) {
+				$settingsLinks[] = [
+					'linktype'  => 'LISTVIEWSETTING',
+					'linklabel' => 'LBL_EDIT_WORKFLOWS',
+					'linkurl'   => 'index.php?parent=Settings&module=Workflows&view=List&sourceModule='.$this->getName(),
+					'linkicon'  => $editWorkflowsImagePath
+				];
 			}
 
-			$settingsLinks[] = array(
-						'linktype' => 'LISTVIEWSETTINGS',
-						'linklabel'=> 'LBL_SERVER_CONFIGURATION',
-						'linkurl' => 'index.php?parent=Settings&module=PBXManager&view=Index',
-						'linkicon'=> ''
-			);
+			$settingsLinks[] = [
+				'linktype' => 'LISTVIEWSETTINGS',
+				'linklabel'=> 'LBL_SERVER_CONFIGURATION',
+				'linkurl'  => 'index.php?parent=Settings&module=PBXManager&view=Index',
+				'linkicon' => ''
+			];
 		}
+
 		return $settingsLinks;
 	}
 
 	/**
 	 * Funxtion to identify if the module supports quick search or not
 	 */
-	public function isQuickSearchEnabled() {
+	public function isQuickSearchEnabled()
+	{
 		return false;
 	}
 
-	public function isListViewNameFieldNavigationEnabled() {
+	public function isListViewNameFieldNavigationEnabled()
+	{
 		return false;
 	}
 
@@ -90,23 +99,25 @@ class PBXManager_Module_Model extends Vtiger_Module_Model {
 	 * Function to check whether the module is an entity type module or not
 	 * @return <Boolean> true/false
 	 */
-	function getUtilityActionsNames() {
-		return array('Import', 'Export', 'Merge');
+	public function getUtilityActionsNames()
+	{
+		return ['Import', 'Export', 'Merge'];
 	}
 
-	public function isWorkflowSupported() {
-                //SalesPlatform.ru start workflow for PBXManager
-                //return false;
+	public function isWorkflowSupported()
+	{
+		//SalesPlatform.ru start workflow for PBXManager
 		return true;
-                //SalesPlatform.ru end
+		//SalesPlatform.ru end
 	}
 
-	function isStarredEnabled(){
+	public function isStarredEnabled()
+	{
 		return false;
 	}
 
-	function isTagsEnabled() {
+	public function isTagsEnabled()
+	{
 		return false;
 	}
 }
-?>
--- modules/PBXManager/views/Detail.php
+++ modules/PBXManager/views/Detail.php
@@ -8,60 +8,37 @@
  * All Rights Reserved.
  * *********************************************************************************** */
 
-class PBXManager_Detail_View extends Vtiger_Detail_View{
-    
-    /**
-     * Overrided to disable Ajax Edit option in Detail View of
-     * PBXManager Record
-     */
-    function isAjaxEnabled($recordModel) {
+class PBXManager_Detail_View extends Vtiger_Detail_View
+{
+	/**
+	 * Overrided to disable Ajax Edit option in Detail View of
+	 * PBXManager Record
+	 * @param mixed $recordModel
+	 */
+	public function isAjaxEnabled($recordModel)
+	{
 		return false;
 	}
- 
-    //SalesPlatform.ru begin
-    function process(\Vtiger_Request $request) {
-        $recordId = $request->get('record');
-        $moduleName = $request->getModule();
-        if (!$this->record) {
-            $this->record = Vtiger_DetailView_Model::getInstance($moduleName, $recordId);
-        }
-        $recordModel = $this->record->getRecord();
 
-        // To show recording link only if callstatus is 'completed' 
-        if ($recordModel->get('callstatus') != 'completed') {
-            $recordModel->set('recordingurl', '');
-        }
-        if ($recordModel->get('callstatus') == 'completed') {
-            $recordModel->set('recordingurl', '<audio src="index.php?module=PBXManager&action=ListenRecord&record=' . $recordId . '" controls>
-                    <a href="index.php?module=PBXManager&action=ListenRecord&record=' . $recordId . '" ><i class="icon-volume-up"></i></a>
+	//SalesPlatform.ru begin
+	public function process(\Vtiger_Request $request)
+	{
+		$recordId = $request->get('record');
+		$moduleName = $request->getModule();
+		if (! $this->record) {
+			$this->record = Vtiger_DetailView_Model::getInstance($moduleName, $recordId);
+		}
+		$recordModel = $this->record->getRecord();
+
+		// To show recording link only if callstatus is 'completed'
+		if ($recordModel->get('callstatus') != 'completed') {
+			$recordModel->set('recordingurl', '');
+		}
+		if ($recordModel->get('callstatus') == 'completed') {
+			$recordModel->set('recordingurl', '<audio src="index.php?module=PBXManager&action=ListenRecord&record='.$recordId.'" controls>
+                    <a href="index.php?module=PBXManager&action=ListenRecord&record='.$recordId.'" ><i class="icon-volume-up"></i></a>
                 </audio>');
-        }
-        parent::process($request);
-    }
-    /*
-     * Overided to convert totalduration to minutes
-     */
-//    function preProcess(Vtiger_Request $request, $display=true) {
-//        $recordId = $request->get('record');
-//        $moduleName = $request->getModule();
-//		if(!$this->record){
-//            $this->record = Vtiger_DetailView_Model::getInstance($moduleName, $recordId);
-//        }
-//        $recordModel = $this->record->getRecord();
-//
-//        // To show recording link only if callstatus is 'completed' 
-//        if($recordModel->get('callstatus') != 'completed') { 
-//            $recordModel->set('recordingurl', '');
-//        }
-//        // SalesPlatform.ru begin
-//        if($recordModel->get('callstatus') == 'completed') {
-//            $recordModel->set('recordingurl', '<audio src="index.php?module=PBXManager&action=ListenRecord&record='.$recordId.'" controls>
-//                    <a href="index.php?module=PBXManager&action=ListenRecord&record='.$recordId.'" ><i class="icon-volume-up"></i></a>
-//                </audio>');
-//        }
-//        // SalesPlatform.ru end
-//
-//        return parent::preProcess($request, true);
-//    }                    
-    //SalesPaltform.ru end
+		}
+		parent::process($request);
+	}
 }
