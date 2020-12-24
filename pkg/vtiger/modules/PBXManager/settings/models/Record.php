e7ca35cfe406d32768683609e1cc990ad7b1f648
dogi <dogi.developer@gmail.com>
2020-12-24 16:34:23 +0100

    fix(PBXManager) Fixed error with variable format

--- pkg/vtiger/modules/PBXManager/settings/models/Record.php
+++ pkg/vtiger/modules/PBXManager/settings/models/Record.php
@@ -6,50 +6,58 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- *************************************************************************************/
+ */
 
-class Settings_PBXManager_Record_Model extends Settings_Vtiger_Record_Model {
+class Settings_PBXManager_Record_Model extends Settings_Vtiger_Record_Model
+{
+	const tableName = 'vtiger_pbxmanager_gateway';
 
-    const tableName = 'vtiger_pbxmanager_gateway';
-    
-    public function getId() {
-        return $this->get('id');
-    }
+	public function getId()
+	{
+		return $this->get('id');
+	}
 
-    public function getName() {
-    }
-    
-    public function getModule(){
-        return new Settings_PBXManager_Module_Model;
-    }
-    
-    static function getCleanInstance(){
-        return new self;
-    }
-    
-     public static function getInstance(){
-        $serverModel = new self();
-        $db = PearDatabase::getInstance();
-        $query = 'SELECT * FROM '.self::tableName;
-        $gatewatResult = $db->pquery($query, array());
-        $gatewatResultCount = $db->num_rows($gatewatResult);
-        
-        if($gatewatResultCount > 0) {
-            $rowData = $db->query_result_rowdata($gatewatResult, 0);
-            $serverModel->set('gateway',$rowData['gateway']);
-            $serverModel->set('id',$rowData['id']);
-            $parameters = Zend_Json::decode(decode_html($rowData['parameters']));
-            foreach ($parameters as $fieldName => $fieldValue) {
-                    $serverModel->set($fieldName,$fieldValue);
-            }
-            return $serverModel;
-        }
-        return $serverModel;
-    }
-    
-    public static function getInstanceById($recordId, $qualifiedModuleName) {
+	public function getName()
+	{
+	}
+
+	public function getModule()
+	{
+		return new Settings_PBXManager_Module_Model;
+	}
+
+	public static function getCleanInstance()
+	{
+		return new self;
+	}
+
+	public static function getInstance()
+	{
+		$serverModel = new self();
 		$db = PearDatabase::getInstance();
-		$result = $db->pquery('SELECT * FROM '.self::tableName.' WHERE id = ?', array($recordId));
+		$query = 'SELECT * FROM '.self::tableName;
+		$gatewatResult = $db->pquery($query, []);
+		$gatewatResultCount = $db->num_rows($gatewatResult);
+
+		if ($gatewatResultCount > 0) {
+			$rowData = $db->query_result_rowdata($gatewatResult, 0);
+			$serverModel->set('gateway', $rowData['gateway']);
+			$serverModel->set('id', $rowData['id']);
+			$parameters = Zend_Json::decode(decode_html($rowData['parameters']));
+			foreach ($parameters as $fieldName => $fieldValue) {
+				$serverModel->set($fieldName, $fieldValue);
+			}
+
+			return $serverModel;
+		}
+
+		return $serverModel;
+	}
+
+	public static function getInstanceById($recordId, $qualifiedModuleName)
+	{
+		$db = PearDatabase::getInstance();
+		$result = $db->pquery('SELECT * FROM '.self::tableName.' WHERE id = ?', [$recordId]);
 
 		if ($db->num_rows($result)) {
 			$moduleModel = Settings_Vtiger_Module_Model::getInstance($qualifiedModuleName);
@@ -62,24 +70,27 @@ class Settings_PBXManager_Record_Model extends Settings_Vtiger_Record_Model {
 			foreach ($parameters as $fieldName => $fieldValue) {
 				$recordModel->set($fieldName, $fieldValue);
 			}
+
 			return $recordModel;
 		}
+
 		return false;
 	}
-    
-    public function save() {
+
+	public function save()
+	{
 		$db = PearDatabase::getInstance();
-		$parameters = '';
+		$parameters = [];
 		$selectedGateway = $this->get('gateway');
-                $connector = new PBXManager_PBXManager_Connector;
-                
-                foreach ($connector->getSettingsParameters() as $field => $type) {
-                        $parameters[$field] = $this->get($field);
-                }
-                $this->set('parameters', Zend_Json::encode($parameters));
-		$params = array($selectedGateway,$this->get('parameters'));
+		$connector = new PBXManager_PBXManager_Connector;
+
+		foreach ($connector->getSettingsParameters() as $field => $type) {
+			$parameters[$field] = $this->get($field);
+		}
+		$this->set('parameters', Zend_Json::encode($parameters));
+		$params = [$selectedGateway, $this->get('parameters')];
 		$id = $this->getId();
-                
+
 		if ($id) {
 			$query = 'UPDATE '.self::tableName.' SET gateway=?, parameters = ? WHERE id = ?';
 			array_push($params, $id);
