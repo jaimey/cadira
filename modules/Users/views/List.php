76d858597faeb556fde0843412694ecfb79b363e
dogi <dogi.developer@gmail.com>
2020-12-27 11:07:08 +0100

    fix(Views) Fixed error in initializeListViewContents function. Code optimization and PSR2 format adaptation

--- modules/Users/views/List.php
+++ modules/Users/views/List.php
@@ -6,108 +6,120 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- ************************************************************************************/
+ */
 
-class Users_List_View extends Settings_Vtiger_List_View {
-
-	function getHeaderScripts(Vtiger_Request $request) {
+class Users_List_View extends Settings_Vtiger_List_View
+{
+	public function getHeaderScripts(Vtiger_Request $request)
+	{
 		$headerScriptInstances = parent::getHeaderScripts($request);
 
-		$jsFileNames = array(
+		$jsFileNames = [
 			'modules.Users.resources.List',
-			"modules.Emails.resources.MassEdit",
-			"~layouts/".Vtiger_Viewer::getDefaultLayoutName()."/lib/jquery/floatThead/jquery.floatThead.js",
-			"~layouts/".Vtiger_Viewer::getDefaultLayoutName()."/lib/jquery/perfect-scrollbar/js/perfect-scrollbar.jquery.js"
-		);
+			'modules.Emails.resources.MassEdit',
+			'~layouts/'.Vtiger_Viewer::getDefaultLayoutName().'/lib/jquery/floatThead/jquery.floatThead.js',
+			'~layouts/'.Vtiger_Viewer::getDefaultLayoutName().'/lib/jquery/perfect-scrollbar/js/perfect-scrollbar.jquery.js'
+		];
 
 		$jsScriptInstances = $this->checkAndConvertJsScripts($jsFileNames);
-		$headerScriptInstances = array_merge($headerScriptInstances, $jsScriptInstances);
-		return $headerScriptInstances;
+
+		return array_merge($headerScriptInstances, $jsScriptInstances);
 	}
 
-	public function process(Vtiger_Request $request) {
+	public function process(Vtiger_Request $request)
+	{
 		$viewer = $this->getViewer($request);
 		$this->initializeListViewContents($request, $viewer);
 		$viewer->view('ListViewContents.tpl', $request->getModule(false));
 	}
 
-	/*
-	 * Function to initialize the required data in smarty to display the List View Contents
-	 */
-	public function initializeListViewContents(Vtiger_Request $request, Vtiger_Viewer $viewer) {
+	// Function to initialize the required data in smarty to display the List View Contents
+	public function initializeListViewContents(Vtiger_Request $request, Vtiger_Viewer $viewer)
+	{
 		$moduleName = $request->getModule();
 		$cvId = $request->get('viewname');
 		$pageNumber = $request->get('page');
 		$orderBy = $request->get('orderby');
 		$sortOrder = $request->get('sortorder');
 		$searchParams = $request->get('search_params');
-		if($sortOrder == "ASC"){
-			$nextSortOrder = "DESC";
-			$sortImage = "icon-chevron-down";
-			$faSortImage = "fa-sort-desc";
-		}else{
-			$nextSortOrder = "ASC";
-			$sortImage = "icon-chevron-up";
-			$faSortImage = "fa-sort-asc";
+
+		if ($sortOrder == 'ASC') {
+			$nextSortOrder = 'DESC';
+			$sortImage = 'icon-chevron-down';
+			$faSortImage = 'fa-sort-desc';
+		} else {
+			$nextSortOrder = 'ASC';
+			$sortImage = 'icon-chevron-up';
+			$faSortImage = 'fa-sort-asc';
 		}
 
-		if(empty ($pageNumber)){
+		if (empty($pageNumber)) {
 			$pageNumber = '1';
 		}
 
 		$status = $request->get('status');
-		if(empty($status))
+		if (empty($status)) {
 			$status = 'Active';
+		}
 
 		$listViewModel = Vtiger_ListView_Model::getInstance($moduleName, $cvId);
 
-		$linkParams = array('MODULE'=>$moduleName, 'ACTION'=>$request->get('view'), 'CVID'=>$cvId);
+		$linkParams = ['MODULE'=>$moduleName, 'ACTION'=>$request->get('view'), 'CVID'=>$cvId];
 		$linkModels = $listViewModel->getListViewMassActions($linkParams);
-				$listViewModel->set('status', $status);
+		$listViewModel->set('status', $status);
 
 		$pagingModel = new Vtiger_Paging_Model();
 		$pagingModel->set('page', $pageNumber);
 
-		if(!empty($orderBy)) {
+		if (! empty($orderBy)) {
 			$listViewModel->set('orderby', $orderBy);
-			$listViewModel->set('sortorder',$sortOrder);
+			$listViewModel->set('sortorder', $sortOrder);
 		}
 
 		$searchKey = $request->get('search_key');
 		$searchValue = $request->get('search_value');
 		$operator = $request->get('operator');
 
-		if($request->get('mode') == 'removeAlphabetSearch') {
-			Vtiger_ListView_Model::deleteParamsSession($moduleName.'_'.$cvId, array('search_key', 'search_value', 'operator'));
+		if ($request->get('mode') == 'removeAlphabetSearch') {
+			Vtiger_ListView_Model::deleteParamsSession($moduleName.'_'.$cvId, ['search_key', 'search_value', 'operator']);
 			$searchKey = '';
 			$searchValue = '';
 			$operator = '';
 		}
-		if($request->get('mode') == 'removeSorting') {
-			Vtiger_ListView_Model::deleteParamsSession($listViewSessionKey, array('orderby', 'sortorder'));
+		if ($request->get('mode') == 'removeSorting') {
+			if (empty($cvId)) {
+				$customView = new CustomView();
+				$cvId = $customView->getViewId($moduleName);
+			}
+
+			$listViewSessionKey = $moduleName.'_'.$cvId;
+			if (! empty($tag)) {
+				$listViewSessionKey .= '_'.$tag;
+			}
+			Vtiger_ListView_Model::deleteParamsSession($listViewSessionKey, ['orderby', 'sortorder']);
 			$orderBy = '';
 			$sortOrder = '';
 		}
-		if(!empty($operator)) {
+		if (! empty($operator)) {
 			$listViewModel->set('operator', $operator);
-			$viewer->assign('OPERATOR',$operator);
-			$viewer->assign('ALPHABET_VALUE',$searchValue);
+			$viewer->assign('OPERATOR', $operator);
+			$viewer->assign('ALPHABET_VALUE', $searchValue);
 		}
-		if(!empty($searchKey) && !empty($searchValue)) {
+		if (! empty($searchKey) && ! empty($searchValue)) {
 			$listViewModel->set('search_key', $searchKey);
 			$listViewModel->set('search_value', $searchValue);
 		}
 
-		if(empty($searchParams)) {
-			$searchParams = array();
+		if (empty($searchParams)) {
+			$searchParams = [];
 		}
 
 		$transformedSearchParams = $this->transferListSearchParamsToFilterCondition($searchParams, $listViewModel->getModule());
-		$listViewModel->set('search_params',$transformedSearchParams);
+		$listViewModel->set('search_params', $transformedSearchParams);
 
 		//To make smarty to get the details easily accesible
-		foreach($searchParams as $fieldListGroup){
-			foreach($fieldListGroup as $fieldSearchInfo){
+		foreach ($searchParams as $fieldListGroup) {
+			foreach ($fieldListGroup as $fieldSearchInfo) {
 				$fieldSearchInfo['searchValue'] = $fieldSearchInfo[2];
 				$fieldSearchInfo['fieldName'] = $fieldName = $fieldSearchInfo[0];
 				$fieldSearchInfo['comparator'] = $fieldSearchInfo[1];
@@ -115,47 +127,44 @@ class Users_List_View extends Settings_Vtiger_List_View {
 			}
 		}
 
-		if(!$this->listViewHeaders){
+		if (! $this->listViewHeaders) {
 			$this->listViewHeaders = $listViewModel->getListViewHeaders();
 		}
-		if(!$this->listViewEntries){
+		if (! $this->listViewEntries) {
 			$this->listViewEntries = $listViewModel->getListViewEntries($pagingModel);
 		}
 		$noOfEntries = count($this->listViewEntries);
 
 		$viewer->assign('MODULE', $moduleName);
 
-		if(!$this->listViewLinks){
+		if (! $this->listViewLinks) {
 			$this->listViewLinks = $listViewModel->getListViewLinks($linkParams);
 		}
+
 		$viewer->assign('LISTVIEW_LINKS', $this->listViewLinks);
-
 		$viewer->assign('LISTVIEW_MASSACTIONS', $linkModels['LISTVIEWMASSACTION']);
-
 		$viewer->assign('PAGING_MODEL', $pagingModel);
-		$viewer->assign('PAGE_NUMBER',$pageNumber);
-
-		$viewer->assign('ORDER_BY',$orderBy);
-		$viewer->assign('SORT_ORDER',$sortOrder);
-		$viewer->assign('NEXT_SORT_ORDER',$nextSortOrder);
-		$viewer->assign('SORT_IMAGE',$sortImage);
-		$viewer->assign('COLUMN_NAME',$orderBy);
+		$viewer->assign('PAGE_NUMBER', $pageNumber);
+		$viewer->assign('ORDER_BY', $orderBy);
+		$viewer->assign('SORT_ORDER', $sortOrder);
+		$viewer->assign('NEXT_SORT_ORDER', $nextSortOrder);
+		$viewer->assign('SORT_IMAGE', $sortImage);
+		$viewer->assign('COLUMN_NAME', $orderBy);
 		$viewer->assign('QUALIFIED_MODULE', $moduleName);
-		$viewer->assign('FASORT_IMAGE',$faSortImage);
-
-		$viewer->assign('LISTVIEW_ENTRIES_COUNT',$noOfEntries);
+		$viewer->assign('FASORT_IMAGE', $faSortImage);
+		$viewer->assign('LISTVIEW_ENTRIES_COUNT', $noOfEntries);
 		$viewer->assign('LISTVIEW_HEADERS', $this->listViewHeaders);
 		$viewer->assign('LISTVIEW_ENTRIES', $this->listViewEntries);
 
 		if (PerformancePrefs::getBoolean('LISTVIEW_COMPUTE_PAGE_COUNT', false)) {
-			if(!$this->listViewCount){
+			if (! $this->listViewCount) {
 				$this->listViewCount = $listViewModel->getListViewCount();
 			}
 			$totalCount = $this->listViewCount;
 			$pageLimit = $pagingModel->getPageLimit();
 			$pageCount = ceil((int) $totalCount / (int) $pageLimit);
 
-			if($pageCount == 0){
+			if ($pageCount == 0) {
 				$pageCount = 1;
 			}
 			$viewer->assign('PAGE_COUNT', $pageCount);
@@ -167,22 +176,23 @@ class Users_List_View extends Settings_Vtiger_List_View {
 		$viewer->assign('USER_MODEL', Users_Record_Model::getCurrentUserModel());
 		$viewer->assign('SEARCH_VALUE', $searchValue);
 		$viewer->assign('SEARCH_DETAILS', $searchParams);
-                
-                $runtime_configs = Vtiger_Runtime_Configs::getInstance();
-                $password_regex = $runtime_configs->getValidationRegex('password_regex');
-                $viewer->assign('PWD_REGEX', $password_regex);
+
+		$runtime_configs = Vtiger_Runtime_Configs::getInstance();
+		$password_regex = $runtime_configs->getValidationRegex('password_regex');
+		$viewer->assign('PWD_REGEX', $password_regex);
 	}
 
 	/**
 	 * Function returns the number of records for the current filter
 	 * @param Vtiger_Request $request
 	 */
-	function getRecordsCount(Vtiger_Request $request) {
+	public function getRecordsCount(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$cvId = $request->get('viewname');
 		$count = $this->getListViewCount($request);
 
-		$result = array();
+		$result = [];
 		$result['module'] = $moduleName;
 		$result['viewname'] = $cvId;
 		$result['count'] = $count;
@@ -197,10 +207,11 @@ class Users_List_View extends Settings_Vtiger_List_View {
 	 * Function to get listView count
 	 * @param Vtiger_Request $request
 	 */
-	function getListViewCount(Vtiger_Request $request){
+	public function getListViewCount(Vtiger_Request $request)
+	{
 		$moduleName = $request->getModule();
 		$cvId = $request->get('viewname');
-		if(empty($cvId)) {
+		if (empty($cvId)) {
 			$cvId = '0';
 		}
 
@@ -208,40 +219,37 @@ class Users_List_View extends Settings_Vtiger_List_View {
 		$searchValue = $request->get('search_value');
 
 		$listViewModel = Vtiger_ListView_Model::getInstance($moduleName, $cvId);
-		if(empty($tagParams)){
-			$tagParams = array();
+		if (empty($tagParams)) {
+			$tagParams = [];
 		}
 
 		$searchParams = $request->get('search_params');
 		$searchAndTagParams = array_merge($searchParams, $tagParams);
 
-		$listViewModel->set('search_params',$this->transferListSearchParamsToFilterCondition($searchAndTagParams, $listViewModel->getModule()));
+		$listViewModel->set('search_params', $this->transferListSearchParamsToFilterCondition($searchAndTagParams, $listViewModel->getModule()));
 
 		$listViewModel->set('search_key', $searchKey);
 		$listViewModel->set('search_value', $searchValue);
 		$listViewModel->set('operator', $request->get('operator'));
 
-		$count = $listViewModel->getListViewCount();
-
-		return $count;
+		return $listViewModel->getListViewCount();
 	}
-
-
 
 	/**
 	 * Function to get the page count for list
 	 * @return total number of pages
 	 */
-	function getPageCount(Vtiger_Request $request){
+	public function getPageCount(Vtiger_Request $request)
+	{
 		$listViewCount = $this->getListViewCount($request);
 		$pagingModel = new Vtiger_Paging_Model();
 		$pageLimit = $pagingModel->getPageLimit();
 		$pageCount = ceil((int) $listViewCount / (int) $pageLimit);
 
-		if($pageCount == 0){
+		if ($pageCount == 0) {
 			$pageCount = 1;
 		}
-		$result = array();
+		$result = [];
 		$result['page'] = $pageCount;
 		$result['numberOfRecords'] = $listViewCount;
 		$response = new Vtiger_Response();
@@ -254,45 +262,48 @@ class Users_List_View extends Settings_Vtiger_List_View {
 	 * @param type $request
 	 * @param type $moduleModel
 	 */
-	public function setModuleInfo($request, $moduleModel){
-		$fieldsInfo = array();
-		$basicLinks = array();
+	public function setModuleInfo($request, $moduleModel)
+	{
+		$fieldsInfo = [];
+		$basicLinks = [];
 
 		$moduleFields = $moduleModel->getFields();
-		foreach($moduleFields as $fieldName => $fieldModel){
+		foreach ($moduleFields as $fieldName => $fieldModel) {
 			$fieldsInfo[$fieldName] = $fieldModel->getFieldInfo();
 		}
 
 		$moduleBasicLinks = $moduleModel->getModuleBasicLinks();
-		foreach($moduleBasicLinks as $basicLink){
+		foreach ($moduleBasicLinks as $basicLink) {
 			$basicLinks[] = Vtiger_Link_Model::getInstanceFromValues($basicLink);
 		}
 
 		$viewer = $this->getViewer($request);
 
-		$listViewModel = Vtiger_ListView_Model::getInstance($moduleModel->getName()); 
-		$linkParams = array('MODULE'=>$moduleModel->getName(), 'ACTION'=>$request->get('view')); 
+		$listViewModel = Vtiger_ListView_Model::getInstance($moduleModel->getName());
+		$linkParams = ['MODULE'=>$moduleModel->getName(), 'ACTION'=>$request->get('view')];
 
-		if(!$this->listViewLinks){ 
-			$this->listViewLinks = $listViewModel->getListViewLinks($linkParams); 
+		if (! $this->listViewLinks) {
+			$this->listViewLinks = $listViewModel->getListViewLinks($linkParams);
 		}
 
-		$viewer->assign('LISTVIEW_LINKS', $this->listViewLinks); 
+		$viewer->assign('LISTVIEW_LINKS', $this->listViewLinks);
 		$viewer->assign('FIELDS_INFO', json_encode($fieldsInfo));
 		$viewer->assign('MODULE_BASIC_ACTIONS', $basicLinks);
 	}
 
-	public function transferListSearchParamsToFilterCondition($listSearchParams, $moduleModel) {
+	public function transferListSearchParamsToFilterCondition($listSearchParams, $moduleModel)
+	{
 		return Vtiger_Util_Helper::transferListSearchParamsToFilterCondition($listSearchParams, $moduleModel);
 	}
 
-	public function getHeaderCss(Vtiger_Request $request) {
+	public function getHeaderCss(Vtiger_Request $request)
+	{
 		$headerCssInstances = parent::getHeaderCss($request);
-		$cssFileNames = array(
-			"~layouts/".Vtiger_Viewer::getDefaultLayoutName()."/lib/jquery/perfect-scrollbar/css/perfect-scrollbar.css",
-		);
+		$cssFileNames = [
+			'~layouts/'.Vtiger_Viewer::getDefaultLayoutName().'/lib/jquery/perfect-scrollbar/css/perfect-scrollbar.css',
+		];
 		$cssInstances = $this->checkAndConvertCssStyles($cssFileNames);
-		$headerCssInstances = array_merge($headerCssInstances, $cssInstances);
-		return $headerCssInstances;
+
+		return array_merge($headerCssInstances, $cssInstances);
 	}
 }
