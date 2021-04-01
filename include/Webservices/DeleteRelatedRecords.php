549b9dac866de1033677c003ed8a62e912c0e41e
dogicrm <dogi.developer@gmail.com>
2021-04-01 02:42:11 +0200

    feat(WebServices) Implemented enhancement to unlink records in potentials module

--- include/Webservices/DeleteRelatedRecords.php
+++ include/Webservices/DeleteRelatedRecords.php
@@ -17,9 +17,11 @@ function vtws_delete_related($sourceRecordId, $relatedRecordId, $user = false)
 {
 	global $log,$adb;
 
+	//Get source record id
 	list($moduleSourceId, $elementSourceId) = vtws_getIdComponents($sourceRecordId);
 	$webserviceObject = VtigerWebserviceObject::fromId($adb, $moduleSourceId);
 
+	//Get instanes handlers
 	$handlerPath = $webserviceObject->getHandlerPath();
 	$handlerClass = $webserviceObject->getHandlerClass();
 
@@ -27,14 +29,18 @@ function vtws_delete_related($sourceRecordId, $relatedRecordId, $user = false)
 	$handler = new $handlerClass($webserviceObject, $user, $adb, $log);
 	$meta = $handler->getMeta();
 
+	//Get objet entity name from record Id.
 	$sourceModuleName = $meta->getObjectEntityName($sourceRecordId);
 
+	//Get module permssion for user.
 	$types = vtws_listtypes(null, $user);
 
+	//Check permissions for user
 	if (! in_array($sourceModuleName, $types['types'])) {
 		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, 'Permission to perform the operation is denied');
 	}
 
+	//Check equal entites
 	if ($sourceModuleName !== $webserviceObject->getEntityName()) {
 		throw new WebServiceException(WebServiceErrorCode::$INVALIDID, 'Id specified is incorrect');
 	}
@@ -43,23 +49,41 @@ function vtws_delete_related($sourceRecordId, $relatedRecordId, $user = false)
 		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, 'Permission to read given object is denied');
 	}
 
+	//Check exist record id
 	if (! $meta->exists($elementSourceId)) {
 		throw new WebServiceException(WebServiceErrorCode::$RECORDNOTFOUND, 'Record you are trying to access is not found');
 	}
 
+	//Check record access
 	if ($meta->hasWriteAccess() !== true) {
 		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, 'Permission to write is denied');
 	}
 
+	//Get related record Id
 	list($moduleRelatedId, $elementRelatedId) = vtws_getIdComponents($relatedRecordId);
+
+	//Get instance for record id
 	$relatedRecordInstance = Vtiger_Record_Model::getInstanceById($elementRelatedId);
 	$relatedModuleName = $relatedRecordInstance->getModuleName();
 
+	//Get instance for source module
 	$sourceModuleFocus = CRMEntity::getInstance($sourceModuleName);
-	if ($sourceModuleFocus) {
-		$sourceModuleFocus->delete_related_module($sourceModuleName, $elementSourceId, $relatedModuleName, $elementRelatedId);
+
+	if ($sourceModuleFocus && $relatedRecordInstance) {
+		//Check modules (source / related) and apply remove link
+		if ($sourceModuleName == 'Potentials' && $relatedModuleName == 'Products') {
+			$query = 'DELETE FROM vtiger_seproductsrel WHERE crmid=? AND productid=? AND setype=?';
+			$adb->pquery($query, [$elementSourceId, $elementRelatedId, 'Potentials']);
+		} elseif ($sourceModuleName == 'Potentials' && $relatedModuleName == 'Contacts') {
+			$query = 'DELETE FROM vtiger_contpotentialrel WHERE potentialid=? AND contactid=?';
+			$adb->pquery($query, [$elementSourceId, $elementRelatedId]);
+		} else {
+			//Default method to remove link
+			$sourceModuleFocus->delete_related_module($sourceModuleName, $elementSourceId, $relatedModuleName, $elementRelatedId);
+		}
 	}
 
+	//Buffer save
 	VTWS_PreserveGlobal::flush();
 
 	return true;
