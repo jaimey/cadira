06b45235da9d6295016bb70e4b6e9c8f33222bb8
dogicrm <dogi.developer@gmail.com>
2021-06-05 18:57:38 +0200

    fix(WebServices) Wrong user reference

--- include/Webservices/LineItem/RetrieveInventory.php
+++ include/Webservices/LineItem/RetrieveInventory.php
@@ -7,26 +7,26 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  *
- *********************************************************************************/
+ */
 
 require_once 'include/Webservices/Retrieve.php';
 
 /**
  * Retrieve inventory record with LineItems
+ * @param mixed $id
  */
-function vtws_retrieve_inventory($id){
+function vtws_retrieve_inventory($id)
+{
 	global $current_user;
 
 	$record = vtws_retrieve($id, $current_user);
 
-	$handler = vtws_getModuleHandlerFromName('LineItem', $user);
-    $id = vtws_getIdComponents($id);
-    $id = $id[1];
+	$handler = vtws_getModuleHandlerFromName('LineItem', $current_user);
+	$id = vtws_getIdComponents($id);
+	$id = $id[1];
 	$inventoryLineItems = $handler->getAllLineItemForParent($id);
 
 	$record['LineItems'] = $inventoryLineItems;
 
 	return $record;
 }
-
-?>
